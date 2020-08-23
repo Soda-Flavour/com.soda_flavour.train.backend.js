3d6d85ed8fef4b324ad09e69497c969d73cee46b
soda-flavour <axa8380@gmail.com>
2020-08-23 23:41:16 +0900

    feat: Set default Screen

--- with_vue_js_socket_io/client/src/App.vue
+++ with_vue_js_socket_io/client/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Landing from "./components/Landing.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    Landing,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ with_vue_js_socket_io/client/src/components/CountDown.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p class="yes-no">No</p>
+    <p class="time-left">{{currentTime}}</p>
+  </div>
+</template>
+
+<script>
+// import { formatDistance } from "date-fns";
+
+export default {
+  data: () => ({
+    currentTime: new Date().toLocaleTimeString(),
+  }),
+  created() {
+    setInterval(() => {
+      this.currentTime = new Date().toLocaleString();
+    }, 500);
+  },
+};
+</script>
+
+<style>
+.yes-no {
+  margin: 2rem;
+  text-align: center;
+  font-size: calc(2rem + 8vmin);
+}
+
+.question .time-left {
+  font-size: calc(1rem + 4vmin);
+}
+</style>
